9aed23b369d9dd78fa429c469780d656257a2ace
Calli Herrmann <calliherrmann@gmail.com>
2023-07-31 17:05:16 -0400

    Add post and delete endpoints

--- server.js
+++ server.js
@@ -15,5 +15,33 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/v1/characters', (req, res) => {
-  res.status(200).json(app.locals.characters);
+    res.status(200).json(app.locals.characters);
+});
+
+app.get('/api/v1/characters/:id', (req, res) => {
+    let id = req.params.id
+    let foundCharacter = app.locals.characters.find(man => man.id === parseInt(id))
+    
+    if (foundCharacter) {
+        res.status(200).json(foundCharacter)
+      } else {
+        res.sendStatus(404)
+      }
+})
+
+app.post('/api/v1/savedreports', (req, res) => {
+    app.locals.savedReports.push(req.body);
+
+    res.status(201).json(app.locals.savedReports);
+});
+
+app.delete('/api/v1/savedreports', (req, res) => {
+    const { id } = req.body;
+    app.locals.savedReports = app.locals.savedReports.filter(task => task.id !== id)
+    
+    res.status(201).json(app.locals.savedReports)
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
